f3ad95174c9cc6c51e09dd1c13a91c82e6276ced
steelburn <steelburn@gmail.com>
2019-07-03 13:07:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,4 +18,4 @@ tail -F /var/log/lighttpd/access.log 2>/dev/null &
 tail -F /var/log/lighttpd/error.log 2>/dev/null 1>&2 &
 lighttpd -D -f /etc/lighttpd/lighttpd.conf &
 
-sleep 10m && watch -n 180 'sh build.sh' &
+sleep 10m && watch -n 180 'sh build.sh'
