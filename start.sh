f9fb3bd992a9faa4fc697e1dae2cff9d56a9cc0b
steelburn <steelburn@gmail.com>
2019-07-03 12:08:06 +0800

    Changing execution order

--- start.sh
+++ start.sh
@@ -14,8 +14,8 @@ fi
 
 # sh ./build.sh
 
-watch -n 180 'sh build.sh' &
-
 tail -F /var/log/lighttpd/access.log 2>/dev/null & 
 tail -F /var/log/lighttpd/error.log 2>/dev/null 1>&2 &
 lighttpd -D -f /etc/lighttpd/lighttpd.conf &
+
+sleep 10m && watch -n 180 'sh build.sh' &
